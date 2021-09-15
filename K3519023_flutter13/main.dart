c3078200fbdb3a6b2f5fc9c3b0196cbc5ee40c81
Dixy-04 <71267358+Dixy-04@users.noreply.github.com>
2021-09-15 21:39:57 +0700

    Add files via upload

---
+++ K3519023_flutter13/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Image Widget"),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.black,
+            width: 200,
+            height: 200,
+            padding: EdgeInsets.all(3),
+            child: Image(
+              //image: NetworkImage("https://ichef.bbci.co.uk/news/800/cpsprodpb/8991/production/_104871253_1a76b03a-0a86-4ee3-85de-46f66d7f179e.jpg"),
+              image: AssetImage("images/gambar.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
